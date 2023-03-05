6aea0b478b8bdea871df0f32491f1e36c9250554
Bohdan Stratila <bogdanstratila@icloud.com>
2023-03-05 13:56:25 +0200

    make commands to be able to run from module where users_db is installed

--- users_db/commands.py
+++ users_db/commands.py
@@ -1,27 +1,48 @@
-"""
-Ref: https://stackoverflow.com/a/74875605
-Also check [tool.poetry.scripts] in pyproject.toml
-"""
+import os
 import click
-import users_db.migrations as migrations
+import contextlib
+
+import users_db
+from users_db import migrations
+
+# We need to change the current working directory to the path where
+# users_db is installed.
+USERS_DB_ABSPATH = os.path.dirname(os.path.abspath(users_db.__file__))
+
+
+@contextlib.contextmanager
+def cd(new_dir):
+    """This is a context manager that changes the current working directory"""
+    prev_dir = os.getcwd()
+    os.chdir(os.path.expanduser(new_dir))
+    try:
+        yield
+    finally:
+        os.chdir(prev_dir)
+
+
+# The idea to use Click module seen in https://stackoverflow.com/a/74875605
 
 
 @click.command()
 @click.option("-v", "--verbose", is_flag=True, default=False, help="Verbose mode")
 def db_current_cmd(verbose):
     """Display current database revision"""
-    migrations.current(verbose)
+    with cd(USERS_DB_ABSPATH):
+        migrations.current(verbose)
 
 
 @click.command()
 @click.option("-r", "--revision", default="head", help="Revision target")
 def db_upgrade_cmd(revision):
     """Upgrade to a later database revision"""
-    migrations.upgrade(revision)
+    with cd(USERS_DB_ABSPATH):
+        migrations.upgrade(revision)
 
 
 @click.command()
 @click.option("-r", "--revision", required=True, help="Revision target")
 def db_downgrade_cmd(revision):
     """Revert to a previous database revision"""
-    migrations.downgrade(revision)
+    with cd(USERS_DB_ABSPATH):
+        migrations.downgrade(revision)
